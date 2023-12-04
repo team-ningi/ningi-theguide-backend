981be434b7bc11736919b4a37235ae41dbfbd355
Gary George <gary@accru.finance>
2023-12-04 23:02:49 +0000

    TEST PROMPT

--- routes/ai.ts
+++ routes/ai.ts
@@ -301,7 +301,7 @@ router.post(
 
       const prePrompt =
         "I want to find out some information, everything i wish to know is inside of this Array of objects,the value in each item is a query.Keep the answers to each query as simple as possible.";
-      const postPrompt = `
+      const postPromptNO = `
       If an answer is available for a query, respond with a JSON object in the format of {'key':'value'} 
       where 'value' is the answer. If the answer is not available for a query, provide a default response of {'key': '' } 
       replacing the value with an empty string. 
@@ -309,9 +309,17 @@ router.post(
       If the information for a query is not available or unknown, respond with {'key': 'Unknown'}.  Please do not send any other response other than a valid JSON Object even if all of the queries do not have answers, always respond with valid JSON.
       `;
 
-      const postPromptOLD =
-        "return the data as a valid JSON object consisting of { 'key': 'value' }, if you dont know an answer for a specific item keep the structure of key:value but make the value be an empty string, if you do know the answer replace the value with the correct data, Keep context, dont return anything you are unsure of. return only the specified JSON object of key: value. Respond ONLY with a Valid JSON object";
-
+      const postPrompt = `Return the data as an object of { 'key': 'value'}, if you dont know an answer for any individual item  
+        please keep the structure of { 'key':'value' } but return the value be an empty string , do not explain to me that you could not answer becuase of lack of context just reply with '' for each individual value you can not answer. 
+        If however, you do know the answer please replace the value with the correct data. Keep context, 
+        dont return anything you are unsure of. Return only the specified JSON object of { 'key': 'value' }. 
+        Respond ONLY with a Valid JSON object, do not respond with text such as 'I Dont Know' or 'Im sorry' , 
+        Respond ONLY with a JSON object with all of the keys present and NO additional text. 
+        If you do not have the answer for a paticular query return an empty string for the value. If you do know the answer put the data in the place of value. 
+        If the provided context does not include the information required to answer a query, then simply return '' as the value. 
+        If the details needed to answer a query is not provided, just return an empty string for that query, If you cant not answer any of the queries then just return the JSON object with all of the keys but with empty strings as the values.
+        To reiterate i only want the valid JSON object returned, i do not want any additional text explaining why the values are empty strings, 
+        ONLY return the valid json object, `;
       const processChunk = async (batch) => {
         const batchStrings = batch.map((obj) => JSON.stringify(obj));
         const theQuery = `${initialPrompt} ${prePrompt} [${batchStrings}] ${postPrompt}`;
